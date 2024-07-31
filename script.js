a75cd3012f4ea62aa09efb4d4a67d53442356f25
Mahmoud Test <mahmoud.attar1997@gmail.com>
2024-07-31 14:00:25 +0300

    local strogeden bilgi alinmasi

--- script.js
+++ script.js
@@ -19,6 +19,9 @@ const amount = document.getElementById('amount');
 const select = document.getElementById('movie');
 const seats = document.querySelectorAll('.seat:not(.reserved)');
 
+getFromLocalStorge();
+calculateTotal();
+
 
 container.addEventListener('click', function(e) {
     if(e.target.classList.contains('seat') && !e.target.classList.contains('reserved')) { 
@@ -57,6 +60,26 @@ function calculateTotal(){
        amount.innerText = selectedSeatCount * select.value;
        saveTolocalStorge(selectedSeatIndexs);
 }
+function getFromLocalStorge(){
+const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+
+if(selectedSeats !=null && selectedSeats.length > 0){
+    seats.forEach(function(seat, index){
+        if(selectedSeats.indexOf(index)>-1){
+            seat.classList.add('selected');
+
+        }
+    });
+}
+
+
+const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+if(selectedMovieIndex !=null){
+    select.selectedIndex = selectedMovieIndex;
+}
+}
+
+
 function saveTolocalStorge(indexs){
     localStorage.setItem('selectedSeats', JSON.stringify(indexs));
     localStorage.setItem('selectedMovieIndex', select.selectedIndex);
